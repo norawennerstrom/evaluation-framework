2d3c8dcd8f98caca9fff39e8f9c229b77f460229
norawennerstrom <nora.wennerstrom@live.com>
2025-05-15 09:47:48 +0200

    kommentarer?

--- src/components/ViewButton.tsx
+++ src/components/ViewButton.tsx
@@ -7,11 +7,15 @@ const ViewButton: React.FC<ViewButtonProps> = ({
   setIsDualView,
   isDualView,
 }) => {
+  // component containing a button for toggling the value of isDualView
+
+  // update button text when variable value changes
   var buttonText = "Single view";
   if (!isDualView) {
     buttonText = "Dual view";
   }
   return (
+    // button inverting the value of isDualView when clicked
     <div
       className="view-buttons"
       onClick={() => setIsDualView((prev: boolean) => !prev)}
